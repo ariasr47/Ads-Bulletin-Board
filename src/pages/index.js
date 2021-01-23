361c697e373bfabebf5068ecc3e9bb95f4001e52
Rodrigo Arias <rodrigo@Rodrigos-MacBook-Pro.local>
2021-01-23 00:45:48 -0800

    fixed mx style property

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Button from "@material-ui/core/Button"
 
 export default function Home() {
   return (
-    <Box mx={auto}>
+    <Box mx={"auto"}>
       <Link to="/Ads">
         <Button color={"secondary"} variant="outlined">
           Proceed
